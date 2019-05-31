4cd72df806471396b12bd231697cacd9d15b5fec
Rudresh <rudreshmishra1993@gmail.com>
2019-05-31 16:01:01 +0200

    second commit

--- challenge3_bitmap_parsing.py
+++ challenge3_bitmap_parsing.py
@@ -250,6 +250,8 @@ new_population_mean = skimage.measure.block_reduce(p,(15,15), np.mean)
 plt.imshow(new_population_mean)  
 
 filtered_population = filtered_image(new_population_mean,40,40,315,140)
+plt.imshow(filtered_population, cmap='gray')
+plt.show()
 
 ####Store the information about human population in numpy array
 Human_population= np.ceil((filtered_population * 3000 * 15 * 15))
@@ -372,7 +374,6 @@ def update_zombies(zombies_position,zombies_prop,next_zombies_position):
         if(partial_zombies[x]<zombies_current[x]):
             partial_zombies[x]+=1
             value-=1
-    print('i got it')
     zombies[next_zombies_position]=[x + y for x, y in zip(partial_zombies, zombies_next)]
     zombies[zombies_position]=[y - x for x, y in zip(partial_zombies, zombies_current)]
 
@@ -425,56 +426,16 @@ def update_human_population(pos,value):
 
 
 def _nested_update(document, key, value,run):
-    if isinstance(document, dict):
+    if document != {}:
         if key in document.keys():
             if run == 0:
                 document[key].update(value[key])
+        run = run - 1
         for dict_key, dict_value in document.items():
-            run = run - 1
-            _nested_update(
-                document=dict_value, key=key, value=value, run=run
-            )
+            _nested_update(document=dict_value, key=key, value=value,run=run)
     return document
 
-######## to perform the step 1####################
 
-def parse_step1(Nodes,run):
-    tree_dict={}
-    List=[]
-    track_count = run+1
-    run = run + 1
-    shift_zombies_per_day()
-    for node in Nodes:
-        Zombies = get_zombies(node)# to get rid of 15 days old zombies since they wont move
-        if(sum(Zombies)>0):
-            tree= tree_dict
-            tree.update({node: {}})
-            x,y = node
-            cells = build_neighbor_pos_list((x,y),filtered_elevation.shape[0], filtered_elevation.shape[1])
-            List.extend(cells)
-            total_population = total_population_nbr_cell(cells)
-            for positions in cells:
-                angle,slope,height_level = calcaulte_elevation(node,positions)
-                nbr_population = calcaute_population(positions)
-                if angle < 10.0 and total_population > 0 and nbr_population>0 and height_level>0:
-                    tree[node].update({positions: {}})
-                    #print((nbr_population,total_population,sum(Zombies),slope))
-                    zombies_prop = int(math.ceil(population_propogate(nbr_population,total_population,sum(Zombies),slope)))
-                    update_zombies(node,zombies_prop,positions)
-            _nested_update(diction, node, tree,run)
-    return List,track_count
-
-
-
-
-def zombies_and_human_step2(Nodes):
-    for node in Nodes:
-        zombies_kills_human(node)
-        
-def human_killing_zombies_step3(Nodes):
-    for node in Nodes:
-        Human_kills_zombies(node)
-        
 
 from collections import Mapping
 # Empty directed graph
@@ -527,6 +488,57 @@ def _nested_lookup(key, document, wild=False, with_keys=False):
                 if isinstance(v, dict):
                     for result in _nested_lookup(key, v, wild=wild, with_keys=with_keys):
                         yield result
+######## to perform the step 1####################
+
+def parse_step1(Nodes,run):
+    tree_dict={}
+    List=[]
+    track_count = run+1
+    run = run + 1
+    shift_zombies_per_day()
+    for node in Nodes:
+        Zombies = get_zombies(node)# to get rid of 15 days old zombies since they wont move
+        if(sum(Zombies)>0):
+            tree= tree_dict
+            tree.update({node: {}})
+            x,y = node
+            cells = build_neighbor_pos_list((x,y),filtered_elevation.shape[0], filtered_elevation.shape[1])
+            List.extend(cells)
+            total_population = total_population_nbr_cell(cells)
+            for positions in cells:
+                angle,slope,height_level = calcaulte_elevation(node,positions)
+                nbr_population = calcaute_population(positions)
+                if angle < 10.0 and total_population > 0 and nbr_population>0 and height_level>0:
+                    tree[node].update({positions: {}})
+                    #print((nbr_population,total_population,sum(Zombies),slope))
+                    zombies_prop = int(math.ceil(population_propogate(nbr_population,total_population,sum(Zombies),slope)))
+                    update_zombies(node,zombies_prop,positions)
+            _nested_update(diction, node, tree,run)
+    return List,track_count
+
+
+
+def zombies_and_human_step2(Nodes):
+    for node in Nodes:
+        zombies_kills_human(node)
+        
+def human_killing_zombies_step3(Nodes):
+    for node in Nodes:
+        Human_kills_zombies(node)
+
+from scipy.spatial import distance
+
+def Short_Distance_finder(List):
+    list2={}
+    node_list=[(48, 4), (49, 5), (50, 6), (50, 7), (49, 8), (50, 9), (51, 10), (51, 11), (52, 12), (51, 13), (52, 14), (53, 15), (52, 16), (53, 17), (52, 18), (51,19), (51, 20), (50, 21), (50, 22), (51, 23), (52, 24), (51, 25), (51, 26), (52, 27), (52, 28), (52, 29), (53, 30), (53, 31), (54, 32), (53, 33),(53, 34), (53, 35), (53, 36), (54, 37), (55, 38), (56, 39), (55, 40), (54, 41), (53, 42), (53, 43), (54, 44), (54, 45), (54, 46), (53, 47), (54, 48),(53, 49), (53, 50), (53, 51), (52, 52), (52, 53), (51, 54), (51, 55), (52, 56), (52, 57), (53, 58), (53, 59), (53, 60), (54, 61), (53, 62), (52, 63),(53, 64), (52, 65), (51, 66), (50, 67), (49, 68), (50, 69), (51, 70), (50, 71), (51, 72), (52, 73), (51, 74), (52, 75), (51, 76), (52, 77), (53, 78),(54, 79), (54, 80), (53, 81), (54, 82), (54, 83), (55, 84), (56, 85), (55, 86), (54, 87), (54, 88), (55, 89), (55, 90), (55, 91), (55, 92), (56, 93),(56, 94), (57, 95), (58, 96), (57, 97), (58, 98), (59, 99), (60, 100), (61, 101), (62, 102), (61, 103), (62, 104), (63, 105), (64, 106), (64, 107),(64, 108), (63, 109), (63, 110), (63, 111), (62, 112), (62, 113), (61, 114), (60, 115), (59, 116), (58, 117), (58, 118), (59, 119), (60, 120), (61,121), (62, 122), (62, 123), (61, 124), (60, 125), (59, 126), (59, 127), (60, 128), (60, 129), (60, 130), (59, 131), (60, 132), (59, 133), (58,134), (59, 135), (58, 136), (59, 137), (60, 138), (61, 139), (62, 140), (62, 141), (63, 142), (62, 143), (63, 144), (62, 145), (63, 146), (64,147), (64, 148), (64, 149), (64, 150), (63, 151), (64, 152), (65, 153), (66, 154), (67, 155), (68, 156), (69, 157), (70, 158), (71, 159), (72,160), (73, 161), (74, 162), (75, 163), (76, 164), (77, 165), (78, 166), (79, 167), (80, 168), (81, 169), (82, 170), (83, 171), (84, 172), (85,173), (86, 174), (87, 175), (88, 176), (89, 177), (90, 178), (91, 179), (92, 180), (93, 181), (94, 182), (95, 183), (96, 184), (97, 185), (98,186), (98, 187), (99, 188), (99, 189), (99, 190), (100, 191), (101, 192), (102, 193), (101, 194), (100, 195), (100, 196), (100, 197), (100, 198),(100, 199), (101, 200), (102, 201), (101, 202), (100, 203), (99, 204), (98, 205), (97, 206), (98, 207), (97, 208), (98, 209), (97, 210), (96, 211),(97, 212), (97, 213), (98, 214), (97, 215), (96, 216), (95, 217), (96, 218), (96, 219), (95, 220), (95, 221), (95, 222), (95, 223), (96, 224), (96,225), (96, 226), (96, 227), (97, 228), (98, 229), (98, 230), (99, 231), (98, 232), (99, 233), (99, 234), (100, 235), (100, 236), (101, 237), (102,238), (101, 239), (101, 240), (101, 241), (102, 242), (102, 243), (101, 244), (101, 245), (101, 246), (102, 247), (101, 248), (101, 249), (101,250), (101, 251), (102, 252), (102, 253), (101, 254)]
+    for nodes in List:
+        dists = [distance.sqeuclidean(nodes, i) for i in node_list]
+        min_dist=min(dists)
+        list2.update({nodes:min_dist })
+    All_node_List = sorted(list2, key=list2.get, reverse=False)
+    return All_node_List[:6]
+       
+#############3basic_prep_for_data#########################ii
 diction={}
 start_pos=(100,255)
 End_pos =(47,4)
@@ -543,13 +555,120 @@ zombies_2d_array = np.sum(zombies, axis=-1)
 
 zombies[100][255][0]=Human_population[100][255]
 
-while(len(nested_lookup((47,4), diction))==0):
+track_previous_node=[]
+
+
+
+def execute_code(List,track):
     List,track = parse_step1(List,track)
-    print(track)
     zombies_and_human_step2(List)
     human_killing_zombies_step3(List)
+    print(track)
+    return List,track
 
 
+while(len(nested_lookup((47,4), diction))==0):
+    List,track=execute_code(List,track)
+    List = Short_Distance_finder(List)
+################################################################################
+    
+#execute the step2 here
+import operator
+def parse_step1_short_distance(Nodes,run,previous_stage_nodes):
+    tree_dict={}
+    List=[]
+    track_count = run+1
+    run = run + 1
+    shift_zombies_per_day()
+    for node in Nodes:
+        Zombies = get_zombies(node)# to get rid of 15 days old zombies since they wont move
+        if(sum(Zombies)>0):
+            tree= tree_dict
+            tree.update({node: {}})
+            list2={}
+            x,y = node
+            cells = build_neighbor_pos_list((x,y),filtered_elevation.shape[0], filtered_elevation.shape[1])
+            total_population = total_population_nbr_cell(cells)
+            global All_node_List
+            global two_stages_nodes
+            two_stages_nodes =previous_stage_nodes
+            print('here is two stage node',two_stages_nodes)
+            for positions in cells:
+                angle,slope,height_level = calcaulte_elevation(node,positions)
+                nbr_population = calcaute_population(positions)
+                if angle < 10.0 and total_population > 0 and nbr_population>0 and height_level>0:
+                    x1,y1 = positions
+                    dist = math.hypot(47 - x1, 4 - y1)
+                    list2.update({positions:dist })
+                    #print((nbr_population,total_population,sum(Zombies),slope))
+                    zombies_prop = int(math.ceil(population_propogate(nbr_population,total_population,sum(Zombies),slope)))
+                    update_zombies(node,zombies_prop,positions)
+                    tree[node].update({positions: {}})
+            min_distance_node =min(list2.items(), key=operator.itemgetter(1))[0]
+            get_zombies(min_distance_node)
+            All_node_List = sorted(list2, key=list2.get, reverse=False)# sort dict in decresing order
+            two_stages_nodes.extend(All_node_List)
+            print('here is also two stage node',two_stages_nodes)
+            List.extend([min_distance_node])
+            _nested_update(diction, node, tree,run)
+            print(node,List)
+    return List,track_count,All_node_List[1:-1],two_stages_nodes[1:-1]
+
+diction={}
+start_pos=(100,255)
+End_pos =(47,4)
+diction.setdefault(start_pos, {})
+List=[(100,255)]
+track =-1
+
+####Store the information about human population in numpy array
+Human_population= np.ceil((filtered_population * 3000 * 15 * 15)).astype(int)
+
+zombies = np.zeros((filtered_elevation.shape[0], filtered_elevation.shape[1],15)).astype(int)
+
+zombies_2d_array = np.sum(zombies, axis=-1)
+
+zombies[100][255][0]=Human_population[100][255]
+
+track_previous_node=[(100,255)]
+
+i=0
+
+##### this is an algorithm with two step back tracking##########################
+
+
+def execute_code_short_distance(List,track,previous_node):
+    global i
+    global track_previous_node
+    global track_second_stage
+    print('prevoisu node',previous_node)
+    List,track,ALL_nodes,two_stage_no = parse_step1_short_distance(List,track,previous_node)
+    print('this is list',List)
+    #print('this is all node',ALL_nodes)
+    print('this is double node',two_stage_no)
+    if (len(List)!=0):
+        i=0
+        zombies_and_human_step2(List)
+        print('i am killing human')
+        human_killing_zombies_step3(List)
+        track_previous_node=ALL_nodes
+        track_second_stage=two_stage_no
+        print('this list you are looking for',track_previous_node)
+        print('this is the double list',track_second_stage)
+    else:
+        
+        print('#########the obtained list were empty so check other value################')
+        List=[track_second_stage[i]]
+        i+=1
+        print("considere this List",List)
+        track-=1
+        execute_code_short_distance(List,track,previous_node=track_second_stage)
+    print(track)
+    return List,track
+        
+while(len(nested_lookup((47,4), diction))==0):
+    List,track=execute_code_short_distance(List,track,track_previous_node)
+        
 
 ###### to crop the image from centre#####
 
@@ -559,3 +678,29 @@ def crop_center(img,cropx,cropy):
     startx = x//2-(cropx//2)
     starty = y//2-(cropy//2)    
     return img[starty:starty+cropy,startx:startx+cropx]  
+
+
+###############################recursive dictionary###############
+def recursive_items(dictionary):
+    for key, value in dictionary.items():
+        if type(value) is dict:
+            yield (key, value)
+            yield from recursive_items(value)
+        else:
+            yield (key, value)
+
+nodes_list=[]
+
+for key, value in recursive_items(diction):
+    nodes_list.extend([key])
+    
+###########################################################3
+    #print nested dictionary in the ordered way
+    
+def pretty(d, indent=0):
+   for key, value in d.items():
+      print('\t' * indent + str(key))
+      if isinstance(value, dict):
+         pretty(value, indent+1)
+      else:
+         print('\t' * (indent+1) + str(value))
